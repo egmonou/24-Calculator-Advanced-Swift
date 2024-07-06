28b0b1cbfb0a49e7a6fce5e188cd37ba12199e22
Nour <mohamednour25@gmail.com>
2024-07-06 13:19:40 +0300

    01

---
+++ Calculator/Controller/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!)else {
+               fatalError("can't convert dispaly labled to a dboule")
+           }
+            return number
+        }set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+  
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-"{
+                //displayLabel.text = String(displayValue * -1)
+                displayValue = displayValue * 1
+            } else if  calcMethod == "AC"{
+                displayLabel.text = "0"
+            }else if  calcMethod == "%"{
+                displayValue = displayValue / 100
+            }
+        }
+    }
+
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        //What should happen when a number is entered into the keypad
+        if let pressedValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text! = pressedValue
+                isFinishedTypingNumber = false
+            }else {
+                if pressedValue == "."{
+   
+                    let isInt = floor(displayValue) == displayValue
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text! += pressedValue
+            }
+        }
+    }
+}
+
---
+++ Calculator/Model/calculatorLogic.swift
@@ -0,0 +1,13 @@
+//
+//  calculatorLogic.swift
+//  Calculator
+//
+//  Created by administrator on 06/07/2024.
+//  Copyright © 2024 London App Brewery. All rights reserved.
+//
+
+import Foundation
+class CalculatorLogic {
+    
+    
+}
